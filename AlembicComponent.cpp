82efa73db5a2a90679234dce5aa2483ce158ddaa
OzanOzkan <ozanozkan@gmail.com>
2018-04-02 15:19:52 +0300

    !FE (AlembicComponent) Play, Pause, Stop features.

---
+++ AlembicComponent.cpp
@@ -0,0 +1,165 @@
+#include "StdAfx.h"
+#include "AlembicComponent.h"
+
+#include <Cry3DEngine/IRenderNode.h>
+
+namespace Cry
+{
+	namespace DefaultComponents
+	{
+		void CAlembicComponent::Register(Schematyc::CEnvRegistrationScope& componentScope)
+		{
+			// Functions
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::Enable, "{97B6CD06-8409-4C86-8AE4-E13F1969CEDD}"_cry_guid, "Enable");
+				pFunction->SetDescription("Enables playback of the alembic component");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				pFunction->BindInput(1, 'enab', "Enable");
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::SetLooping, "{712B5FDD-96BC-4C2A-ACB7-A35252ACD9D2}"_cry_guid, "SetLooping");
+				pFunction->SetDescription("Makes the alembic component loop playback");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				pFunction->BindInput(1, 'loop', "Loop");
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::IsLooping, "{221752A5-051D-4C90-B51F-C669C0EC14AD}"_cry_guid, "IsLooping");
+				pFunction->SetDescription("Is the alembic component looping plyback?");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				pFunction->BindOutput(0, 'loop', "Looping");
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::SetPlaybackTime, "{5B435614-8E6C-428A-8641-7CD13442B054}"_cry_guid, "SetPlaybackTime");
+				pFunction->SetDescription("Skips to a certain time in the animation");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				pFunction->BindInput(1, 'time', "Time");
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::GetPlaybackTime, "{C5ECE743-88CF-4A51-AAE0-105012AC7453}"_cry_guid, "GetPlaybackTime");
+				pFunction->SetDescription("Gets the current playback time");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				pFunction->BindOutput(0, 'time', "Time");
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::Play, "{C91CB65E-4400-4382-A8C5-7C447A3A16E9}"_cry_guid, "Play");
+				pFunction->SetDescription("Plays the alembic animation");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::Pause, "{34087A40-704B-44D4-A237-0F0A1EC34B69}"_cry_guid, "Pause");
+				pFunction->SetDescription("Pauses the alembic animation");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				componentScope.Register(pFunction);
+			}
+			{
+				auto pFunction = SCHEMATYC_MAKE_ENV_FUNCTION(&CAlembicComponent::Stop, "{EC63B1C4-6A09-4F87-9B51-C1EA86EFC734}"_cry_guid, "Stop");
+				pFunction->SetDescription("Stops the alembic animation");
+				pFunction->SetFlags(Schematyc::EEnvFunctionFlags::Construction);
+				componentScope.Register(pFunction);
+			}
+		}
+
+		void CAlembicComponent::Initialize()
+		{
+			if (m_filePath.value.size() > 0)
+			{
+				m_pEntity->LoadGeomCache(GetOrMakeEntitySlotId(), m_filePath.value);
+			}
+		}
+
+		void CAlembicComponent::ProcessEvent(SEntityEvent& event)
+		{
+			if (event.event == ENTITY_EVENT_COMPONENT_PROPERTY_CHANGED)
+			{
+				Initialize();
+			}
+			else if (event.event == ENTITY_EVENT_UPDATE)
+			{
+				SetPlaybackTime(GetPlaybackTime() + m_playSpeed);
+			}
+		}
+
+		uint64 CAlembicComponent::GetEventMask() const
+		{
+			uint64 bitFlags = m_isPlayEnabled ? BIT64(ENTITY_EVENT_UPDATE) : 0;
+			bitFlags |= BIT64(ENTITY_EVENT_COMPONENT_PROPERTY_CHANGED);
+
+			return bitFlags;
+		}
+
+		void CAlembicComponent::Enable(bool bEnable)
+		{
+			if (IGeomCacheRenderNode* pRenderNode = m_pEntity->GetGeomCacheRenderNode(GetEntitySlotId()))
+			{
+				pRenderNode->SetDrawing(bEnable);
+			}
+		}
+
+		void CAlembicComponent::SetLooping(bool bLooping)
+		{
+			if (IGeomCacheRenderNode* pRenderNode = m_pEntity->GetGeomCacheRenderNode(GetEntitySlotId()))
+			{
+				pRenderNode->SetLooping(bLooping);
+			}
+		}
+
+		bool CAlembicComponent::IsLooping() const
+		{
+			if (IGeomCacheRenderNode* pRenderNode = m_pEntity->GetGeomCacheRenderNode(GetEntitySlotId()))
+			{
+				return pRenderNode->IsLooping();
+			}
+
+			return false;
+		}
+
+		void CAlembicComponent::SetPlaybackTime(float time)
+		{
+			if (IGeomCacheRenderNode* pRenderNode = m_pEntity->GetGeomCacheRenderNode(GetEntitySlotId()))
+			{
+				pRenderNode->SetPlaybackTime(time);
+			}
+		}
+
+		float CAlembicComponent::GetPlaybackTime() const
+		{
+			if (IGeomCacheRenderNode* pRenderNode = m_pEntity->GetGeomCacheRenderNode(GetEntitySlotId()))
+			{
+				return pRenderNode->GetPlaybackTime();
+			}
+
+			return 0.f;
+		}
+
+		void CAlembicComponent::SetFilePath(const char* szFilePath)
+		{
+			m_filePath = szFilePath;
+		}
+
+		void CAlembicComponent::Play()
+		{
+			m_isPlayEnabled = true;
+			m_pEntity->UpdateComponentEventMask(this);
+		}
+
+		void CAlembicComponent::Pause()
+		{
+			m_isPlayEnabled = false;
+			m_pEntity->UpdateComponentEventMask(this);
+		}
+
+		void CAlembicComponent::Stop()
+		{
+			m_isPlayEnabled = false;
+			m_currentTime = 0.01f;
+			SetPlaybackTime(0.f);
+			m_pEntity->UpdateComponentEventMask(this);
+		}
+	}
+}
---
+++ AlembicComponent.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <CryRenderer/IRenderer.h>
+#include <CryRenderer/IShader.h>
+
+#include <CrySchematyc/ResourceTypes.h>
+#include <CrySchematyc/MathTypes.h>
+#include <CrySchematyc/Env/IEnvRegistrar.h>
+
+class CPlugin_CryDefaultEntities;
+
+namespace Cry
+{
+	namespace DefaultComponents
+	{
+		class CAlembicComponent
+			: public IEntityComponent
+		{
+		protected:
+			friend CPlugin_CryDefaultEntities;
+			static void Register(Schematyc::CEnvRegistrationScope& componentScope);
+
+			// IEntityComponent
+			virtual void   Initialize() final;
+
+			virtual void   ProcessEvent(SEntityEvent& event) final;
+			virtual uint64 GetEventMask() const final;
+			// ~IEntityComponent
+
+		public:
+			CAlembicComponent() {}
+			virtual ~CAlembicComponent() {}
+
+			static void ReflectType(Schematyc::CTypeDesc<CAlembicComponent>& desc)
+			{
+				desc.SetGUID("{32B8795F-C9A7-4162-B71F-E514C65093F6}"_cry_guid);
+				desc.SetEditorCategory("Geometry");
+				desc.SetLabel("Alembic Mesh");
+				desc.SetDescription("A component containing an alembic mesh");
+				desc.SetIcon("icons:ObjectTypes/object.ico");
+				desc.SetComponentFlags({ IEntityComponent::EFlags::Transform, IEntityComponent::EFlags::Socket, IEntityComponent::EFlags::Attach });
+
+				desc.AddMember(&CAlembicComponent::m_filePath, 'file', "FilePath", "File", "Determines the geom cache file (abc / cbc) to load", "%ENGINE%/EngineAssets/GeomCaches/default.cbc");
+				desc.AddMember(&CAlembicComponent::m_playSpeed, 'pspd', "PlaySpeed", "Speed", "Determines the play speed of the animation", 0.005f);
+			}
+
+			virtual void Enable(bool bEnable);
+			virtual void SetLooping(bool bLooping);
+			virtual bool IsLooping() const;
+
+			virtual void SetPlaybackTime(float time);
+			virtual float GetPlaybackTime() const;
+
+			virtual void SetFilePath(const char* szFilePath);
+			const char* GetFilePath() const { return m_filePath.value.c_str(); }
+
+			virtual void Play();
+			virtual void Pause();
+			virtual void Stop();
+
+		protected:
+			Schematyc::GeomCacheFileName m_filePath;
+			bool m_isPlayEnabled = false;
+			float m_playSpeed = 0.005f;
+			float m_currentTime = 0.01f;
+		};
+	}
+}
